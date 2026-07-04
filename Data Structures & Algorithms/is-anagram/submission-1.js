27363aec32bfe81d93900c9bc002d759066cddf5
Asad <masadullah8055@gmail.com>
2026-07-04 14:38:52 +0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const hashTable = {};
+
+        for (let i = 0; i < s.length; i++) {
+            if (!hashTable[s[i]]) {
+                hashTable[s[i]] = 0;
+            }
+            hashTable[s[i]]++;
+        }
+
+        for (let j = 0; j < t.length; j++) {
+            if (!hashTable[t[j]]) {
+                return false;
+            }
+            hashTable[t[j]]--;
+        }
+
+        return true;
+    }
+}
